e9bebbc2527efaeec2cf866a627f66642ab844e7
tesfalem tekie <teshanna72016@gmail.com>
2022-11-14 10:15:23 -0600

    join_exercise done

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,47 +1,40 @@
 USE employees;
 
-# SELECT CONCAT(employees.first_name, ' ', employees.last_name) AS Department_Manager, departments.dept_name AS Department_Name
-# FROM employees
-# JOIN dept_manager ON employees.emp_no = dept_manager.emp_no
-# JOIN departments ON departments.dept_no = dept_manager.dept_no;
-
 # Each department along with the name of the current manager for that department.
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
-FROM  employees e
-  JOIN dept_manager dm on e.emp_no = dm.emp_no
-  JOIN departments d on dm.dept_no = d.dept_no
+FROM employees e
+    JOIN dept_manager dm ON e.emp_no = dm.emp_no
+    JOIN departments d ON dm.dept_no = d.dept_no
 WHERE dm.to_date LIKE '9%'
 ORDER BY d.dept_name;
 
 # Find the name of all departments currently managed by women.
 SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
-FROM  employees e
-    JOIN dept_manager dm on e.emp_no = dm.emp_no
-    JOIN departments d on dm.dept_no = d.dept_no
+FROM employees e
+    JOIN dept_manager dm ON e.emp_no = dm.emp_no
+    JOIN departments d ON dm.dept_no = d.dept_no
 WHERE dm.to_date LIKE '9%'
     AND e.gender = 'F'
 ORDER BY d.dept_name;
 
-
 # Find the current titles of employees currently working in the Customer Service department.
-SELECT title, COUNT(title)
+SELECT t.title, COUNT(title)
 FROM titles t
-    JOIN employees e on e.emp_no = t.emp_no
-    JOIN dept_emp de on e.emp_no = de.emp_no
+    JOIN employees e ON e.emp_no = t.emp_no
+    JOIN dept_emp de ON e.emp_no = de.emp_no
 WHERE de.dept_no LIKE '%9'
   AND t.to_date LIKE '9%'
   AND de.to_date LIKE '9%'
 GROUP BY title ORDER BY COUNT(title) DESC;
 
-
 # Find the current salary of all current managers.
 SELECT d.dept_name AS 'Department Name',
-       CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager',
-        s.salary
-FROM  salaries s
-  JOIN employees e on e.emp_no = s.emp_no
-  JOIN dept_manager dm on e.emp_no = dm.emp_no
-  JOIN departments d on dm.dept_no = d.dept_no
+    CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager',
+    s.salary
+FROM salaries s
+  JOIN employees e ON e.emp_no = s.emp_no
+  JOIN dept_manager dm ON e.emp_no = dm.emp_no
+  JOIN departments d ON dm.dept_no = d.dept_no
 WHERE dm.to_date LIKE '9%'
     AND s.to_date LIKE '9%'
 ORDER BY d.dept_name;
